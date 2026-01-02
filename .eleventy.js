2b0eee63694775bf547add56565f8eccdd5b8a2a
Mike Mathew <m2mathew@me.com>
2026-01-01 22:49:21 -0600

    refactor: add plugins for lightningcss and navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,21 @@
 import shortcodes from './config/shortcodes.js'
+import plugins from './config/plugins.js'
 
 export default (async function (eleventyConfig) {
+  // passthrough
+  ['src/assets'].forEach(path => {
+    eleventyConfig.addPassthroughCopy(path)
+  })
+
+  // plugins
+  plugins.forEach(plugin => {
+    eleventyConfig.addPlugin(plugin.name, { ...plugin.options })
+  })
+
   // shortcodes
-    Object.keys(shortcodes).forEach(shortcodeName => {
-        eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName])
-    })
+  Object.keys(shortcodes).forEach(shortcodeName => {
+    eleventyConfig.addShortcode(shortcodeName, shortcodes[shortcodeName])
+  })
 
   eleventyConfig.configureErrorReporting({ allowMissingExtensions: true })
 
---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+import lightningcss from '@11tyrocks/eleventy-plugin-lightningcss';
+import navigation from '@11ty/eleventy-navigation';
+
+export default [
+  {
+    name: lightningcss,
+  },
+  {
+    name: navigation,
+  },
+];
