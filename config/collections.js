44cbf8a1fbaf14312de42048a227efe789f2dd62
Mike Mathew <m2mathew@me.com>
2026-01-08 16:51:26 -0600

    refactor: add collections file with only blog posts done so far

---
+++ config/collections.js
@@ -0,0 +1,26 @@
+// create the url for given content type
+const buildPath = (type) => {
+  return `src/posts/${type}/**/*.md`;
+};
+
+// blog post
+export const posts = (eleventyCollection) => {
+  let posts = eleventyCollection
+    .getFilteredByGlob(buildPath('blog'))
+    .filter((p) => {
+      return !p.data.rssClub;
+    })
+    .reverse();
+
+  for (let i = 0; i < posts.length; i += 1) {
+    const nextPost = posts[i - 1];
+    const prevPost = posts[i + 1];
+    posts[i].data['nextPost'] = nextPost;
+    posts[i].data['prevPost'] = prevPost;
+  }
+  return posts;
+};
+
+export default {
+  posts,
+};
