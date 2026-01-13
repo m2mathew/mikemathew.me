0ab8f65a84ac79d6895c5f77ec91a48c4a6f9883
Mike Mathew <m2mathew@me.com>
2026-01-13 08:49:33 -0600

    refactor: update explore pages and data

---
+++ src/_data/site/explore.js
@@ -0,0 +1,160 @@
+export default function() {
+	return {
+		stuff: [
+			{
+				title: 'About',
+				link: '/about',
+				description: 'Learn about Mike',
+				icon: 'about',
+			},
+			{
+				title: 'Blog',
+				link: '/blog',
+				description: 'Post that are longer',
+				icon: 'blog',
+			},
+			// {
+			// 	title: 'Style Guide',
+			// 	link: '/styleguide',
+			// 	description: 'My style guide',
+			// },
+			// {
+			// 	title: 'Notes',
+			// 	link: '/notes',
+			// 	description: 'notes, toots, and shitposts',
+			// 	icon: 'notes',
+			// },
+			{
+				title: 'Links',
+				link: '/links',
+				description: 'A collection of links that I might want to reference later',
+				icon: 'links',
+			},
+			// {
+			// 	title: 'Almanac',
+			// 	link: '/almanac',
+			// 	description: 'A media blog - everything I\'ve watched, read, and played',
+			// 	icon: 'almanac',
+			// },
+			// {
+			// 	title: 'Everything',
+			// 	link: '/explore/everything',
+			// 	description: 'Every post from my blog, notes, links, and Almanac',
+			// 	icon: 'everything',
+			// },
+			{
+				title: 'Subscribe',
+				link: '/subscribe',
+				description: 'Subscribe to posts from this site',
+				icon: 'rss',
+			},
+			{
+				title: 'Projects',
+				link: '/projects',
+				description: 'Things I\'ve built',
+				icon: 'projects',
+			},
+			// {
+			// 	title: 'Intersect',
+			// 	link: '/intersect',
+			// 	description: 'A wiki of sorts. Everything I know, mostly',
+			// 	icon: 'intersect',
+			// },
+			// {
+			// 	title: 'Collections',
+			// 	link: '/collections',
+			// 	description: 'A catalog of my collections',
+			// 	icon: 'collections',
+			// 	more: [
+			// 		{
+			// 			title: 'Lego',
+			// 			link: '/collections/lego',
+			// 			description: 'My Lego collection',
+			// 			icon: 'lego',
+			// 		},
+			// 		{
+			// 			title: 'Games',
+			// 			link: '/collections/games',
+			// 			description: 'My game collection',
+			// 			icon: 'games',
+			// 		},
+			// 		{
+			// 			title: 'Comics',
+			// 			link: '/collections/comics',
+			// 			description: 'My comics collection',
+			// 			icon: 'comics',
+			// 		}
+			// 	]
+			// },
+		// 	{
+		// 		title: 'Podcasts',
+		// 		link: '/podcasts',
+		// 		description: 'My podcasts, my listening history, and a podroll',
+		// 		icon: 'podcasts',
+		// 	},
+		// 	{
+		// 		title: 'Log',
+		// 		link: '/log',
+		// 		description: 'Changelog and previous versions of this site',
+		// 		icon: 'log',
+		// 	},
+		],
+		slash: [
+			{
+				title: 'ai',
+				link: '/ai',
+				description: 'What I do and don\'t use AI for',
+			},
+			{
+				title: 'now',
+				link: '/now',
+				description: 'What I\'m up to now',
+			},
+			// {
+			// 	title: 'next',
+			// 	link: '/next',
+			// 	description: 'What I\'m up to next',
+			// },
+			// {
+			// 	title: 'uses',
+			// 	link: '/uses',
+			// 	description: 'All the things I use on a day-to-day basis',
+			// },
+			// {
+			// 	title: 'defaults',
+			// 	link: '/defaults',
+			// 	description: 'My app defaults',
+			// },
+			// {
+			// 	title: 'interests',
+			// 	link: '/interests',
+			// 	description: 'Things I\'m interested in, passionate about, or otherwise enjoy',
+			// },
+			{
+				title: 'posse',
+				link: '/posse',
+				description: 'My POSSE strategy',
+			},
+			// {
+			// 	title: 'save',
+			// 	link: '/save',
+			// 	description: 'Use these referral links for services I use and we both get free stuff',
+			// },
+			{
+				title: 'contact',
+				link: '/contact',
+				description: 'How to contact me',
+			},
+			{
+				title: 'verify',
+				link: '/verify',
+				description: 'Verify my identities',
+			},
+			{
+				title: 'blank',
+				link: '/blank',
+				description: '',
+			},
+		],
+	}
+}
